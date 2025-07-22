e5286dcf2faf3fa98483f9c89d72763e538ebb1c
eyo-as <eyosiyastumiso7@gmail.com>
2025-07-22 16:24:54 +0300

    respond the token

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -106,7 +106,7 @@ async function login(req, res) {
     return res.status(200).json({
       message: "Login successfull",
       success: true,
-      token,
+      token: token,
     });
   } catch (error) {
     console.error(error.message);
